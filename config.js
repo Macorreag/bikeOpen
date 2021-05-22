4db1f68d407097325470ab339a8cf6ce6b652d0a
Miller Correa <macorreag@unal.edu.co>
2021-05-22 00:23:19 -0500

    add new reverse direction to show a user

---
+++ config.js
@@ -0,0 +1,5 @@
+var config = {
+    // API_From https://developers.arcgis.com
+    ARC_GIS : "AAPKce44f1c52b424250907ba5c56e52e78dZm-j5eswKhmZKi4AbEr_J_j5AU11PHtGI1tJvteJT2BmaY3Z73ailQTmjDg46OhQ",
+    SECRET_API_KEY : '56789',
+  }
